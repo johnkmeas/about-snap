f5381183cc4f4918cc0b0622203b3e82e161d4ee
johnkmeas <johnkmeas@gmail.com>
2019-07-25 01:01:43 -0700

    add about us and contact form to fornt page

--- functions/custom-shortcodes.php/button.php
+++ functions/custom-shortcodes.php/button.php
@@ -7,8 +7,7 @@ function snap_button_function( $atts, $content = null ) {
         'url' => 'my own url here'
 	), $atts, 'button' );
 
-    return '<a href="'. site_url() . $atts[$url] .'" class="button">' . $content . '</a>';
+    return '<a href="'. site_url() .'/'. $atts['url'] .'" class="button">' . $content . '</a>';
 }
-
 
 add_shortcode('button', 'snap_button_function');
--- parts/loop-landing-page.php
+++ parts/loop-landing-page.php
@@ -5,11 +5,13 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
-						
+
+  <!-- About the Fold -->
 	<header class="article-header">
 		<h1 class="page-title"><?php the_title(); ?></h1>
 	</header> <!-- end article header -->
-					
+
+  <!-- Hero Section -->
   <section class="entry-content inner-content grid-x grid-margin-x grid-padding-x" itemprop="text">
     <div class="small-12 large-6 medium-8 cell">
       <?php  the_field('hero_content'); ?>
@@ -19,11 +21,14 @@
     </div>
 	</section> <!-- end article section -->
 
+  <!-- Shout Out -->
   <div class="column text-center callout large">
     <h2>
       <?php  the_field('main_heading'); ?></div>
     </h2>
   </div>
+
+  <!-- Main Section -->
   <section class="entry-content inner-content grid-x grid-margin-x grid-padding-x" itemprop="text">
     <div class="small-12 large-6 medium-8 cell">
       <?php  the_field('main_content'); ?>
@@ -64,6 +69,8 @@
       include(locate_template('parts/loop-set.php'));
     ?>
   </section>
+
+  <!-- Events Section -->
   <section>
     <?php 
     if( have_rows('event_types') ): ?>
@@ -89,6 +96,65 @@
       <?php endwhile; ?>
       </ul>
     <?php endif; ?>
+  </section>
+
+  <!-- About Us Section -->
+  <section>
+    <div>
+      <h2>
+        <?php the_field('about_title') ?>
+      </h2>
+      <?php $about = get_field('about_excerpt');
+        
+        if( $about ): 
+    
+        $post = $about;
+        setup_postdata( $post ); 
+      ?>
+        <div>
+          <div>
+            <?php the_excerpt(); ?>
+            <a href="<?php the_permalink(); ?>">More <?php the_title(); ?></a>
+          </div>
+          <?php the_post_thumbnail(); ?>
+        </div>
+        <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
+      <?php endif; ?>
+    </div>
+  </section>
+
+  <!-- Contact Us -->
+  <section class="entry-content inner-content grid-x grid-margin-x grid-padding-x" itemprop="text">
+    <div class="small-12 large-6 medium-8 cell">
+      <?php 
+        $contact_message =  get_field('contact_us_message');
+        
+        if($contact_message !== '' ){
+          echo $contact_message;
+        }else {
+          $the_slug = 'contact-us';
+          $args = array(
+            'name'        => $the_slug,
+            'post_type'   => 'page'
+          );
+          $post = get_posts($args)[0];
+          if( $post ) :
+            setup_postdata( $post );  ?>
+            <h2><?php the_title(); ?></h2>
+            <div>
+              <div>
+                <p><?php the_excerpt(); ?></p>
+              </div>
+              <?php the_post_thumbnail(); ?>
+            </div>
+            <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly
+          endif;
+        }
+      ?>
+      <?php 
+      $contact_submit = get_field('contact_cta_title') ?: 'contact us' ;
+      echo do_shortcode("[contact-form-7 id='162' title='Contact form 1' submit 'contact']"); ?>
+    </div>
   </section>
 
 	<footer class="article-footer">
