efba8e3005feb3b4cf1fc3a224219730ea8b36dc
johnkmeas <johnkmeas@gmail.com>
2019-07-28 23:34:29 -0700

    sticky nav

--- assets/scripts/js/custom.js
+++ assets/scripts/js/custom.js
@@ -35,4 +35,17 @@ jQuery(document).ready(function($) {
       // instead of a settings object
     ]
   });
+
+ 
+   $(document).ready(function(){
+     $(window).bind('scroll', function() {
+     var navHeight = $( window ).height() - 70;
+       if ($(window).scrollTop() > navHeight) {
+         $('.js-fix-scroll').addClass('stick-top-nav');
+       }
+       else {
+         $('.js-fix-scroll').removeClass('stick-top-nav');
+       }
+    });
+  });
 });
