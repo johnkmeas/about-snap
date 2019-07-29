38f9a5f4d682d74594d01fd379d6d28e75576adb
johnkmeas <johnkmeas@gmail.com>
2019-07-28 20:40:16 -0700

    fonts loaded

--- parts/loop-gallery.php
+++ parts/loop-gallery.php
@@ -1,8 +1,8 @@
 <?php
-  $size = 'full'; // (thumbnail, medium, large, full or custom size)
+  $size = 'large'; // (thumbnail, medium, large, full or custom size)
 
   if( $gallery ): ?>
-    <ul class="event-gallery slick-slider"">
+    <ul class="event-gallery slick-slider bg-gallery">
         <?php foreach( $gallery as $image ): ?>
             <li>
               <?php echo wp_get_attachment_image( $image['ID'], $size ); ?>
